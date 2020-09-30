430686352bba9e9d6123f41d51dd307c61aba7a4
Sunil Adsul <71579266+suniladsul53@users.noreply.github.com>
2020-09-30 11:34:32 +0530

    Add files via upload

---
+++ Reddit.com_Automation/G1ANT.Addon.Reddit/Commands/RedditLoginCommand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Reddit
+{
+    [Command(Name = "reddit.login", Tooltip = "This command opens reddit in a web browser provided in the Argument.")]
+    public class RedditloginCommand : Command
+    {
+        public RedditloginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the Login e-mail ID here")]
+            public TextStructure email { get; set; }
+
+            [Argument(Required = true, Tooltip = "Enter the password here")]
+            public TextStructure pword { get; set; }
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                        "chrome",
+                        "reddit.com",
+                        arguments.Timeout.Value,
+                        false,
+                        Scripter.Log,
+                        Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+                arguments.Search.Value = "/html/body/div[1]/div/div[2]/div[2]/div/div/div/div[2]/div[2]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "username";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.email.Value, arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "password";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.pword.Value, arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+                var len = SeleniumManager.CurrentWrapper.RunScript("return document.getElementsByClassName(\"captcha -internal\").length");
+                if (len == "1")
+                {
+                    RobotMessageBox.Show("Captcha detected, please solve the captcha");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+
+
+}
