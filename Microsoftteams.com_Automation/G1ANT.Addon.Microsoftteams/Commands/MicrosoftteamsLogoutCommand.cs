44e86bb553a7bbc80106afab482f12a8aaea3d2e
Sunil Adsul <71579266+suniladsul53@users.noreply.github.com>
2020-09-30 11:31:27 +0530

    Add files via upload

---
+++ Microsoftteams.com_Automation/G1ANT.Addon.Microsoftteams/Commands/MicrosoftteamsLogoutCommand.cs
@@ -0,0 +1,37 @@
+﻿using G1ANT.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Microsoftteams
+{
+    [Command(Name = "microsoftteams.logout", Tooltip = "This command logout from the msteams account.")]
+    public class MicrosoftteamsLogoutCommand : Command
+    {
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public MicrosoftteamsLogoutCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while closing selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ Microsoftteams.com_Automation/G1ANT.Addon.Microsoftteams/Commands/MicrosoftteamsSearchCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Microsoftteams
+{
+    [Command(Name = "microsoftteams.search", Tooltip = "This command search any text on microsoftteams search box")]
+    class MicrosoftteamsSearchCommand : Command
+    {
+        public MicrosoftteamsSearchCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Text to be typed")]
+            public TextStructure Text { get; set; }
+
+            [Argument(Tooltip = "If set to `true`, the command should wait for a new window to appear after clicking the specified element")]
+            public BooleanStructure WaitForNewWindow { get; set; } = new BooleanStructure(false);
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "provide xpath here";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.Text.Value, arguments, arguments.Timeout.Value);
+
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while typing text to element. Text: '{arguments.Text.Value}'. 'Search element phrase: '{arguments.Search.Value}', by: '{arguments.By.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
